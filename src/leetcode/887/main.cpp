9170b92001fd2dd86f2950a6536d1220a14abe9d
simon <simonzgx@gmail.com>
2020-04-11 11:25:06 +0800

    add leetcode

---
+++ src/leetcode/887/main.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int superEggDrop(int K, int N) {
+        int ret = 1;
+        unsigned int i = 1;
+        while(i<N){
+            ret++;
+        }
+
+        return ret;
+    }
+};
+
+
+int main(){
+    return 0;
+}
