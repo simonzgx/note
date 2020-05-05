8992c7aacaf1609dc3050f94d4b8a7158038f7e5
Simon <simonzgx@gmail.com>
2020-05-05 21:30:59 +0800

    add leetcode 98

---
+++ src/leetcode/98/main.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Simon on 2020/3/28.
+//
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode *root) {
+        bool ans = false;
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+
+}
+
+
