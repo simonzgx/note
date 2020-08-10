a025f65c76ea9206f8ae6136c0abcca846fde1d6
Simon <simonzgx@gmail.com>
2020-08-10 18:08:59 +0800

    add socket

--- src/net/socket.cpp
+++ src/net/socket.cpp
@@ -3,3 +3,86 @@
 //
 
 #include "socket.h"
+
+net::Socket::Socket(int af, int sock_type, int protocol) {
+    this->m_fd = socket(af, sock_type, protocol);
+    if (this->m_fd < 0) {
+        char err[ERR_MSG_LEN];
+        NetSetError(err, "create socket error: %s(errno: %d)\n", strerror(errno), errno);
+        perror(err);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int net::Socket::SetReuseAddr(char *err_msg) {
+    int yes = 1;
+    if (setsockopt(this->m_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        NetSetError(err_msg, "setsockopt SO_REUSEADDR: %s", strerror(errno));
+        return NET_ERR;
+    }
+    return NET_OK;
+}
+
+net::Socket::~Socket() {
+    if (m_fd != -1) {
+        close(m_fd);
+    }
+}
+
+net::TCPServer::TCPServer(int port, const char *bind_addr) : m_port(port) {
+    int rv;
+    char _port[6];
+    struct addrinfo hints{}, *serv_info;
+    char err[ERR_MSG_LEN];
+
+    snprintf(_port,6,"%d",port);
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((rv = getaddrinfo(bind_addr, _port, &hints, &serv_info)) != 0) {
+        NetSetError(err, "%s", gai_strerror(rv));
+        perror(err);
+        exit(EXIT_FAILURE);
+    }
+
+    this->m_socket = std::make_shared<Socket>(serv_info->ai_family, serv_info->ai_socktype, serv_info->ai_protocol);
+    do {
+        if (this->m_socket->SetReuseAddr(err) != NET_OK) {
+            break;
+        }
+
+        if (this->Listen(serv_info, err) != NET_OK) {
+            break;
+        }
+        freeaddrinfo(serv_info);
+        return;
+    } while (false);
+
+    perror(err);
+    freeaddrinfo(serv_info);
+    exit(EXIT_FAILURE);
+}
+
+int net::TCPServer::Listen(const struct addrinfo *addr, char *err_msg) {
+    if (bind(this->m_socket->m_fd, addr->ai_addr, addr->ai_addrlen) == -1) {
+        NetSetError(err_msg, "bind: %s", strerror(errno));
+        return NET_ERR;
+    }
+
+    if (listen(this->m_socket->m_fd, 10) == -1) {
+        NetSetError(err_msg, "listen: %s", strerror(errno));
+        return NET_ERR;
+    }
+    return NET_OK;
+}
+
+int net::TCPServer::Accept(char *err_msg) {
+    int conn_fd = accept(this->m_socket->m_fd, static_cast<struct sockaddr *>(nullptr), nullptr);
+    if (conn_fd == -1) {
+        NetSetError(err_msg, "accept socket error: %s(errno: %d)", strerror(errno), errno);
+        return NET_ERR;
+    }
+    return conn_fd;
+}
--- src/net/socket.h
+++ src/net/socket.h
@@ -5,12 +5,105 @@
 #ifndef NET_SOCKET_H
 #define NET_SOCKET_H
 
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <cstdint>
+#include <cstdarg>
+#include <cerrno>
+#include <memory>
+#include <vector>
+
+#if defined(__MSVCRT__) || defined(_MSC_VER) || defined(WIN32)
+#endif
+
+#ifdef linux
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#endif
+
+#ifdef apple
+#endif
 
 
-class socket {
+namespace net {
 
-};
+#define NET_OK 0
+#define NET_ERR -1
+#define ERR_MSG_LEN 256
 
+    static void NetSetError(char *err, const char *fmt, ...) {
+        va_list ap;
+
+        if (!err) return;
+        va_start(ap, fmt);
+        vsnprintf(err, ERR_MSG_LEN, fmt, ap);
+        va_end(ap);
+    }
+
+    class Socket {
+    public:
+        explicit Socket(int af, int sock_type, int protocol);
+
+        virtual ~Socket();
+
+        virtual int Init(char *err_msg){};
+
+        virtual int Read(char *buf, size_t buff_len){};
+
+        virtual int Write(char *buf, size_t count){};
+
+        virtual int SetNonBlock(char *err_msg){};
+
+        virtual int SetBlock(char *err_msg){};
+
+        virtual int SetReuseAddr(char *err_msg);
+
+        int m_fd{};
+    };
+
+    using SocketPTR = std::shared_ptr<Socket>;
+
+    class Server {
+    public:
+        Server() = default;
+
+        virtual int Listen(SocketPTR, char *err_msg) = 0;
+
+        virtual int Accept(char *err_msg) = 0;
+    };
+
+    class Client {
+        Client();
+
+        int Connect();
+    };
+
+
+    class TCPServer  {
+
+    public:
+        TCPServer(int port, const char *bind_addr);
+
+        int Listen(const struct addrinfo *addr,char *err_msg) ;
+
+        int Accept(char *err_msg) ;
+
+    private:
+        int m_port;
+
+        SocketPTR m_socket;
+    };
+
+    class TCPClient : public Client {
+        TCPClient();
+    };
+}
 
 
 #endif //NET_SOCKET_H
