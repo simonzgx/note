e20df72693b56626a8f7b69fdca28b4521f38103
Simon <simonzgx@gmail.com>
2020-09-08 15:53:39 +0800

    delete files

--- src/python/cmp_table.py
+++
@@ -1,7 +0,0 @@
-import pymysql
-import pandas as pd
-import sqlalchemy
-from util import comp
-
-if __name__ == "__main__":
-    exit(comp("", ""))
--- src/python/util.py
+++
@@ -1,9 +0,0 @@
-import pymysql
-import pandas as pd
-import sqlalchemy
-import mysql_connector
-
-
-def comp(table1:str, table2:str) ->int:
-    
-    return 0
