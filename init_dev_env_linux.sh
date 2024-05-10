6cb3be11c1bce957dd0c4b0aaa010b591ca75ef5
Xu Zhang <simonzgx@gmail.com>
2024-05-10 17:42:22 +0800

    add global flags to git config

--- init_dev_env_linux.sh
+++ init_dev_env_linux.sh
@@ -4,6 +4,9 @@ set -ex
 
 script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+git config --global user.name "Xu Zhang"
+git config --global user.email "simonzgx@gmail.com"
+
 sudo apt update
 sudo apt -y install zsh cmake
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
--- init_omz.sh
+++ init_omz.sh
@@ -6,6 +6,3 @@ cp $script_path/.zshrc ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-git config user.name "Xu Zhang"
-git config user.email "simonzgx@gmail.com"
