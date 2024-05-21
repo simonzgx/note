38b88910c6dc1b14f57822311dad112b2d4da2d9
Xu Zhang <simonzgx@gmail.com>
2024-05-21 13:12:25 +0800

    install psutil

--- init_dev_env_linux.sh
+++ init_dev_env_linux.sh
@@ -9,5 +9,8 @@ git config --global user.email "simonzgx@gmail.com"
 
 sudo apt update
 sudo apt -y install zsh cmake ninja-build clangd ccache lld clang-format clang
+
+pip install psutil
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
